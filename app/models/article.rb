bd3f505c9daebf414fe1788360433eb34c3788b3
ArseniyXaoc <arseniy@aplaut.com>
2022-01-19 10:02:25 +0300

    add actions

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article
   include Mongoid::Document
   include Mongoid::Timestamps
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
   field :title, type: String
   field :body, type: String
 end
